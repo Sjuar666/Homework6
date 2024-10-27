8e94fba75e5a277fc58a487d08a2361f832b01f9
Samuel Juarez <sjuar845@gmail.com>
2024-10-27 02:12:06 -0500

    Added score chane template

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
-
+  
 </script>
 
 <template>
-  <NavMenu />
+  
   <RouterView />
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,29 +1,29 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import BaseTitle from '@/components/BaseTitle.vue'
+import BaseTitle from '@/components/BaseTitle.vue';
 import useAPI from '@/composables/useAPI'
+import MainScore from '@/components/MainScore.vue';
 
 const { categories, getCategories } = useAPI()
 
-onMounted(async () =>  {
+onMounted(async () => {
   await getCategories()
 })
-
-
 
 
 </script>
 
 <template>
-  <BaseTitle>TRIVIA APPLICATION</BaseTitle>
+  <BaseTitle>TRIVIA APPLICATION - <MainScore></MainScore> </BaseTitle>
   <div class="grid flex-grow grid-cols-4 gap-12 m-20">
     <RouterLink v-for="category in categories" 
     :key="category.id"
-    :to="` /question/category/${category.id}`"
-    class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase hover:cursor-pointer hover:border-red-500 hover:bg-red-400 hover:text-white transation-colors duration-300"
+    :to="`/question/category/${category.id}`"
+    class="bg-white text-center flex h-32 items-center justify-center rounded-lg border-4 border-slate-500 py-4 font-bold uppercase hover:cursor-pointer hover:border-red-500 hover:bg-red-400 hover:text-white transition-colors duration-300"
     >
-      {{ category.name }}
+      {{  category.name }}
     </RouterLink>
-  </div>
+  </div>  
 
+ 
 </template>
